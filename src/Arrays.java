98c4b52f75c4d4c4f457ddd1c9ef20ca7c7c36bc
Nguyễn Khắc Qui <hivongmotloihua@gmail.com>
2020-09-11 11:44:23 +0700

    sua lai

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
         int Array[] = {1, 5, 8, 4, 6, 7, 9, 2, 3};
-
-        for (int i = 0; i < Array.length; i++) {
-            if (Array[i] == 5) {
-                Array[i] = Array[i + 1];
-                i++;
-            }
-            System.out.println(Array[i]);
+        Scanner sc = new Scanner(System.in);
+        int index;
+        do {
+            System.out.println("Enter index delete : ");
+            index = sc.nextInt();
+        } while (index < 0 || index > Array.length-1);
+        for (int i = index; i < Array.length - 1; i++) {
+            Array[i] = Array[i + 1];
         }
-
+        Array[Array.length - 1] = 0;
+        for (int i = 0; i < Array.length; i++) {
+            System.out.print(Array[i] + " ");
+        }
     }
 }
